70928e493200ad454bdda5231f35c50c010689d4
PatrykBlacha <pblacha@student.agh.edu.pl>
2026-01-08 17:55:01 +0100

    wypozyczenia rezerwacje

--- web/app.js
+++ web/app.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:8081/api";
+const API = "http://localhost:8081/api/users";
 
 document.getElementById("registerForm").addEventListener("submit", async (e) => {
     e.preventDefault();
